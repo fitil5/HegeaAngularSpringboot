db7d7e718c359c34a81f94bfaa6d40730a5d5a7a
Ignaciovpcl <108610070+fitil5@users.noreply.github.com>
2023-01-28 21:00:54 +0100

    002

--- SpringBootJwtAuthentication/src/main/java/com/hegea/jwtauthentication/repository/AltaActividadRepository.java
+++ SpringBootJwtAuthentication/src/main/java/com/hegea/jwtauthentication/repository/AltaActividadRepository.java
@@ -15,7 +15,7 @@ import com.hegea.jwtauthentication.services.IActividad;
 
 
 public interface AltaActividadRepository extends JpaRepository<AltaActividadCompositeKey, AltaActividad>{
-	@Query("select new com.grokonez.jwtauthentication.model.AltaActividadDTO(f.fecha, f.nombre_act,a.usuario,a.alta) from FechaActividadCompositeKey f left join AltaActividadCompositeKey a ON a.fecha = f.fecha and a.nombre_act = f.nombre_act  and a.usuario=?1 where f.fecha > current_date  order by f.fecha")
+	@Query("select new com.hegea.jwtauthentication.model.AltaActividadDTO(f.fecha, f.nombre_act,a.usuario,a.alta) from FechaActividadCompositeKey f left join AltaActividadCompositeKey a ON a.fecha = f.fecha and a.nombre_act = f.nombre_act  and a.usuario=?1 where f.fecha > current_date  order by f.fecha")
 	List<AltaActividadDTO> findFechaActividadJoinAltaActividad(String usuario);
 	@Query("select aac from AltaActividadCompositeKey aac where aac.fecha =?1 and aac.nombre_act=?2")
 	List<AltaActividadCompositeKey> findAllByFechaActividad(Date fecha,String actividad);
